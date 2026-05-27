b232cc72a8ff279454b4d26961ce7fe4ffa40991
MigueLunaa007 <252310055@itslerdo.edu.mx>
2026-05-26 20:07:22 -0600

    Añade actividad integradora sobre gestión de bitácoras en archivos de texto

---
+++ Actividad Integradora - Gestion de Bitacoras en Archivos de Texto/codigo/bitacora/index.php
@@ -0,0 +1,7 @@
+<?php
+// inicializamos las variables para mensajes
+$mensaje = "";
+$tipo_mensaje = "";
+$archivo = "bitacora.txt";
+
+//1. Procesamos el formulario al enviar (Validacion y Escritura)
